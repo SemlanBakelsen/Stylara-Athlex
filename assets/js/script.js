f3b49cb44452cd666b7220f6f0c95803307b91f0
SemlanBakelsen <samisamuel42@gmail.com>
2023-07-26 11:03:41 +0200

    edit javaScript for menu button dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
-const menuButton = document.querySelector('.menu-button');
-const dropdownMenu = document.querySelector('.dropdown-menu');
+function myFunction() {
+  document.querySelector('.dropdown-menu').classList.toggle('show');
+}
 
-menuButton.addEventListener('click', () => {
-  dropdownMenu.classList.toggle('show');
-});
-
-// Close the dropdown menu when a menu item is clicked
-const menuItems = document.querySelectorAll('.dropdown-menu ul li a');
-menuItems.forEach((menuItem) => {
-  menuItem.addEventListener('click', () => {
-    dropdownMenu.classList.remove('show');
-  });
-});
-
-// Close the dropdown menu when clicking outside the menu
+// Close the dropdown menu if the user clicks outside of it
 document.addEventListener('click', (event) => {
-  if (!dropdownMenu.contains(event.target) && !menuButton.contains(event.target)) {
-    dropdownMenu.classList.remove('show');
+  if (!event.target.matches('.menu-button')) {
+        var dropdowns = document.getElementsByClassName('dropdown-menu');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
 });
